2ee5a48b61ecf51c5043ca63eb29b27c51eb7d1c
Michael Douglas <134800430+MichaelDouglas2@users.noreply.github.com>
2024-09-27 15:28:08 -0300

    Update jogo.py
    aprimorando o código do jogo, adicionado 3 níveis de dificuldade e agora o número secreto é gerado aleatoriamente.

--- jogo.py
+++ jogo.py
@@ -1,4 +1,8 @@
-12
+import random
+
+#variáveis globais
+secretNumber = random.randint(1, 100) #O número secreto varia entre 1 e 100
+
 def abertura():
     print('============================================')
     print('             HOUSE OF GENIUS                ')
@@ -6,24 +10,46 @@ def abertura():
     print('============================================')
 
 
-#main
+def nivel():
+
+    print('[1] Fácil')
+    print('[2] Médio')
+    print('[3] Difícil')
+    escolhaNivel = int(input('Escolha o nível de dificuldade: '))
+
+    dificuldade = 0
+
+    if escolhaNivel == 1:
+        dificuldade = 15 # 15 tentativas no nivel fácil
+    elif escolhaNivel == 2:
+        dificuldade = 10 # 10 tentativas no nível médio
+    elif escolhaNivel == 3:
+        dificuldade = 5 # 5 tentativas no nível difícil
+
+    return dificuldade
+
 
 def jogo():
-    abertura()
 
-    secretNumber = 12
+    numeroTentativas = nivel()
 
-    tentativa = int(input("Digite um número entre 1 a 100: "))
+    while numeroTentativas > 0:
 
-    if tentativa == secretNumber: 
-        print('Parabéns, voce acertou!')
-    else: 
-        print('Você errou, tente novamente!')
+        tentativa = int(input("Digite um número entre 1 a 100: "))
 
-    continua = input('Deseja continuar? [S/N]: ') 
+        if tentativa == secretNumber:
+            print('\nParabéns, voce acertou!')
+            break
+        else: 
+            if tentativa > secretNumber:
+                print('Seu chute foi maior que o número secreto\n')
+            else:
+                print('Seu chute foi menor que o número secreto\n')
+
+        numeroTentativas -= 1
 
 
 if __name__ == '__main__':
     
+    abertura()
     jogo()
-
