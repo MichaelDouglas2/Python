1fd7efc075088e6f77d1736b9ac944727be5b466
Michael Douglas <134800430+MichaelDouglas2@users.noreply.github.com>
2024-10-07 17:36:10 -0300

    Update ex003.py

--- ex003.py
+++ ex003.py
@@ -1,7 +1,11 @@
 primeiro_valor = input('Digite um valor: ')
 segundo_valor = input('Digite outro valor: ')
 
-if(primeiro_valor > segundo_valor):
-    print(f'O primeiro valor = {primeiro_valor} é maior que o {segundo_valor}')
+if(primeiro_valor >= segundo_valor):
+    print(f'O primeiro valor = {primeiro_valor} ' 
+          f' é maior que o segundo valor = {segundo_valor}'
+    )
 else:
-    print(f'O segundo valor = {segundo_valor} é maior que o primeiro valor = {primeiro_valor}')
+    print(f'O segundo valor = {segundo_valor} '
+          f'é maior que o primeiro valor = {primeiro_valor}'
+    )
