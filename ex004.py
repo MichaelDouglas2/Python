736f42dadffff443860c9060350db1dea9fad23f
Michael Douglas <134800430+MichaelDouglas2@users.noreply.github.com>
2024-10-08 14:35:03 -0300

    Update ex004.py

--- ex004.py
+++ ex004.py
@@ -4,7 +4,12 @@ idade = input('Digite sua idade: ')
 if(nome != '' and idade != ''):
     print(f'Seu nome é {nome}')
     print(f'Seu nome invertido é {nome[:: -1]}')
-    print(f'Seu nome tem espaços: {' ' in nome}')
+
+    if ' ' in nome:
+        print('Seu nome tem espaços')
+    else: 
+        print('Seu nome não contem espaços')
+    
     print(f'Seu nome tem {len(nome)} letras')
     print(f'A primeira letra do seu nome é: {nome[0]}')
     print(f'A última letra do seu nome é {nome[-1]}')
