5878641915ec2e03153459125d268b29105e4de4
Michael Douglas <134800430+MichaelDouglas2@users.noreply.github.com>
2024-10-09 15:08:44 -0300

    Função Len

---
+++ ex007.py
@@ -0,0 +1,10 @@
+nome = input('Digite seu nome: ')
+
+numero_letras = len(nome)
+
+if numero_letras < 5:
+    print('Seu nome é curto')
+elif numero_letras < 7:
+    print('Seu nome é normal')
+else:
+    print('Seu nome é muito grande')
