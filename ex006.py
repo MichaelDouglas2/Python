0063b1b8165118f75d871adba3851b266de9b000
Michael Douglas <134800430+MichaelDouglas2@users.noreply.github.com>
2024-10-09 15:03:17 -0300

    IF - ELIF  -ELSE

---
+++ ex006.py
@@ -0,0 +1,13 @@
+horario = input('Digite o horário: ')
+horario_int = int(horario)
+
+bom_dia = horario_int >= 0 and horario_int < 12
+boa_tarde = horario_int >= 12 and horario_int < 18
+boa_noite = horario_int >= 18 and horario_int < 24
+
+if bom_dia:
+    print('Bom dia!')
+elif boa_tarde:
+    print('Boa tarde!')
+else: 
+    print('Boa noite!')
