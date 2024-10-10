a5718e22ed7808a0b2dd387f855b16f0910e635c
Michael Douglas <134800430+MichaelDouglas2@users.noreply.github.com>
2024-10-09 23:05:32 -0300

    Update ex007.py

--- ex007.py
+++ ex007.py
@@ -2,9 +2,14 @@ nome = input('Digite seu nome: ')
 
 numero_letras = len(nome)
 
-if numero_letras < 5:
-    print('Seu nome é curto')
-elif numero_letras < 7:
-    print('Seu nome é normal')
+if numero_letras > 1:
+    
+    if numero_letras < 5:
+        print('Seu nome é curto')
+    elif numero_letras < 7:
+        print('Seu nome é normal')
+    else:
+        print('Seu nome é muito grande')
+
 else:
-    print('Seu nome é muito grande')
+    print('Digite mais de uma letra')
