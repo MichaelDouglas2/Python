82f0c30b4e58bad2882c79063a3484d86be2acf9
Michael Douglas <134800430+MichaelDouglas2@users.noreply.github.com>
2024-10-09 23:01:29 -0300

    Update ex006.py

--- ex006.py
+++ ex006.py
@@ -1,13 +1,18 @@
 horario = input('Digite o horário: ')
-horario_int = int(horario)
 
-bom_dia = horario_int >= 0 and horario_int < 12
-boa_tarde = horario_int >= 12 and horario_int < 18
-boa_noite = horario_int >= 18 and horario_int < 24
+try:
+    horario_int = int(horario)
 
-if bom_dia:
-    print('Bom dia!')
-elif boa_tarde:
-    print('Boa tarde!')
-else: 
-    print('Boa noite!')
+    bom_dia = horario_int >= 0 and horario_int < 12
+    boa_tarde = horario_int >= 12 and horario_int < 18
+    #boa_noite = horario_int >= 18 and horario_int < 24
+
+    if bom_dia:
+        print('Bom dia!')
+    elif boa_tarde:
+        print('Boa tarde!')
+    else: 
+        print('Boa noite!')
+
+except:
+    print('Você não digitou um número')
