18816e0f277aa5c7989e0b22a7b6b57c96bc5f7c
Michael Douglas <134800430+MichaelDouglas2@users.noreply.github.com>
2024-10-10 17:25:47 -0300

    Calculadora com while

---
+++ ex009.py
@@ -0,0 +1,25 @@
+### calculadora while
+
+while True:
+
+    print(" -----  CALCULADORA ----- ")
+    n1 = input("Digite o primeiro número: ")
+    primeiro_numero = int(n1)
+
+    n2 = input("Digite o segundo número: ")
+    segundo_numero = int(n2)
+
+    escolha = input('Escolha a operação: \n[1] adição\n[2] subtração\n[3] multiplicação\n[4] divisão\n[5] sair\n Escolha: ')
+    operacao = int(escolha)
+
+    if operacao == 1:
+        print(f' a soma entre {primeiro_numero} e {segundo_numero} é {primeiro_numero + segundo_numero}')
+    elif operacao == 2:
+        print(f' a subtração entre {primeiro_numero} e {segundo_numero} é {primeiro_numero - segundo_numero}')
+    elif operacao == 3:
+        print(f' a multiplicação entre {primeiro_numero} e {segundo_numero} é {primeiro_numero * segundo_numero}')
+    elif operacao == 4:
+        print(f' a divisão entre {primeiro_numero} e {segundo_numero} é {primeiro_numero / segundo_numero}')
+    else: 
+        print('Saindo...')
+        break
