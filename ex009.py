7d87bda248617fa0697ffd89d510269471f9e80f
Michael Douglas <134800430+MichaelDouglas2@users.noreply.github.com>
2024-10-11 14:09:51 -0300

    Calculadora com While

--- ex009.py
+++ ex009.py
@@ -3,28 +3,53 @@
 while True:
 
     print(" -----  CALCULADORA ----- ")
-    n1 = input("Digite o primeiro número: ")
-    primeiro_numero = int(n1)
 
-    n2 = input("Digite o segundo número: ")
-    segundo_numero = int(n2)
+    #tratando a entrada
+    try:
+        n1 = input("Digite o primeiro número: ")
+        primeiro_numero = float(n1)
+
+        n2 = input("Digite o segundo número: ")
+        segundo_numero = float(n2)
+    except:
+        print('Você não digitou um número')
+        continue
 
     escolha = input('Escolha a operação: \n[1] adição\n[2] subtração\n[3] multiplicação\n[4] divisão\n Escolha: ')
-    operacao = int(escolha)
+    
+    try: 
+        operacao = int(escolha)
+    except:
+        print('Você não escolheu um operador')
+        continue
 
     if operacao == 1:
-        print(f' a soma entre {primeiro_numero} e {segundo_numero} é {primeiro_numero + segundo_numero}')
+        print(f'a soma entre {primeiro_numero} e {segundo_numero} é {primeiro_numero + segundo_numero}')
+    
     elif operacao == 2:
-        print(f' a subtração entre {primeiro_numero} e {segundo_numero} é {primeiro_numero - segundo_numero}')
+        print(f'a subtração entre {primeiro_numero} e {segundo_numero} é {primeiro_numero - segundo_numero}')
+    
     elif operacao == 3:
-        print(f' a multiplicação entre {primeiro_numero} e {segundo_numero} é {primeiro_numero * segundo_numero}')
+        print(f'a multiplicação entre {primeiro_numero} e {segundo_numero} é {primeiro_numero * segundo_numero}')
+    
     elif operacao == 4:
-        print(f' a divisão entre {primeiro_numero} e {segundo_numero} é {primeiro_numero / segundo_numero}')
+    
+        try:
+            print(f'a divisão entre {primeiro_numero} e {segundo_numero} é {primeiro_numero / segundo_numero}')
+        except ZeroDivisionError:
+            print("Divisao por zero nao existe")
+            continue
     else: 
         print('Operação inválida...')
         continue
 
     sair = input('Deseja sair? [s/n]: ')
 
-    if sair == 's':
+    if sair in 'Ss':
         break
+    elif sair in 'Nn':
+       continue
+    else:
+        print('Opção invalida')
+        continue
+   
